b6c13bf7485de0a89344a1520b56d17c8e361b82
Eddy Meals <edimea211@gmail.com>
2025-11-21 23:20:41 -0800

    Implement CLI

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
+import sys
+
+def panic(msg):
+    print(msg, file=sys.stderr)
+    exit(1)
+
 def main():
-    print('Hello world!')
+    if len(sys.argv) < 2:
+        panic('Missing polynomial degree')
+    try:
+        degree = int(sys.argv[2])
+    except ValueError:
+        panic('Invalid polynomial degree')
+    if len(sys.argv) > 3:
+        panic('Too many arguments')
+    if len(sys.argv) == 3:
+        try:
+            file = open(sys.argv[2], 'r')
+        except FileNotFoundError:
+            panic('Failed to open input file')
+    else:
+        file = sys.stdin
+    points = []
+    for line in file.readlines():
+        try:
+            point = (float(x) for x in line.split(','))
+        except ValueError:
+            panic('Found non-numeric data')
+        if len(point) != 2:
+            panic('Too many fields for data point')
+        points.append(point)
+    
+    for coeff in fit(degree, points):
+        print(coeff)
 
 if __name__ == '__main__':
     main()
