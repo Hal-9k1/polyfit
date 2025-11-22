c093f00b46019f188c75210e44505677886b82c5
Eddy Meals <edimea211@gmail.com>
2025-11-21 23:52:43 -0800

    Start changing CLI to loop-based

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
 import sys
+import os
+
+DEFAULT_TRIALS = 1
+DEFAULT_STEPS = 
 
 def panic(msg):
     print(msg, file=sys.stderr)
     exit(1)
 
 def main():
-    if len(sys.argv) < 2:
-        panic('Missing polynomial degree')
-    try:
-        degree = int(sys.argv[2])
-    except ValueError:
-        panic('Invalid polynomial degree')
-    if len(sys.argv) > 3:
-        panic('Too many arguments')
-    if len(sys.argv) == 3:
+    degree = None
+    seed = None
+    trials = DEFAULT_TRIALS
+    steps = None
+    file = None
+    for arg in sys.argv:
+        if arg.startswith('--degree='):
+            try:
+                degree = 
+
+    if file != None:
         try:
             file = open(sys.argv[2], 'r')
         except FileNotFoundError:
             panic('Failed to open input file')
     else:
         file = sys.stdin
+
     points = []
     for line in file.readlines():
         try:
@@ -29,9 +36,29 @@ def main():
         if len(point) != 2:
             panic('Too many fields for data point')
         points.append(point)
+
+    random.seed(os.environ.get('SEED', 0))
     
-    for coeff in fit(degree, points):
+    for coeff in fit(degree, points, os.environ.get('TRIALS', 1)):
         print(coeff)
+
+def fit(degree, data, trials, steps):
+    return min(
+        [_do_fit(degree, data) for _ in range(trials)],
+        key=lambda x: _get_sq_error(x, data)
+    )
+
+def _do_fit(degree, data, steps):
+    # least degree first
+    coeffs = [0 for _ in range(degree)]
+    for 
+
+def _get_sq_error(coeffs, data):
+    total = 0
+    for point in data:
+        predicted = [k * pow(point[0], n) for n, k in enumerate(coeffs)]
+        total += pow(predicted - point[1], 2)
+    return total
 
 if __name__ == '__main__':
     main()
