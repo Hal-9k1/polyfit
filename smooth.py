2aaa952ca561430d145975b6a9febee067f13a84
Eddy Meals <edimea211@gmail.com>
2025-11-22 11:27:57 -0800

    Start implementing smooth.py

--- smooth.py
+++ smooth.py
@@ -3,12 +3,20 @@ from cli_util import panic, parse_args, pos_int
 
 DEFAULT_MAX_PROCESSES = 1
 
-HELP_TEXT = '''foobar
+HELP_TEXT = '''Applies Svitzsky-Golay smoothing to a 2D dataset.
 
 '''
 
-def smooth():
-    pass
+def smooth(degree, data, trials, steps, max_perturb, window, max_subprocs, smooth_procs):
+    if max_subprocs < 1:
+        max_subprocs = os.process_cpu_count()
+    if smooth_procs < 1:
+        smooth_procs = os.process_cpu_count()
+    fit_procs = max_subprocs // smooth_procs
+
+    smoothable_start = window // 2
+    smoothable_end = len(data) - window // 2
+    fit(degree, data, trials, steps, max_perturb, fit_procs)
 
 def _run_cli():
     named, positional = parse_args({
@@ -20,6 +28,7 @@ def _run_cli():
         'scanprocs': int,
         'perturb': float,
         'window': pos_int,
+        'output': str,
         'help': None,
     })
 
@@ -30,7 +39,8 @@ def _run_cli():
     max_subprocs = named.get('maxprocs', DEFAULT_MAX_PROCESSES)
     smooth_procs = named.get('scanprocs', DEFAULT_MAX_PROCESSES)
     max_perturb = named.get('perturb')
-    filename = positional[0] if len(positional) else None
+    in_filename = positional[0] if len(positional) else None
+    out_filename = named.get('output')
 
     if 'help' in named:
         print(HELP_TEXT, file=sys.stderr)
@@ -51,17 +61,29 @@ def _run_cli():
     if len(positional) > 1:
         panic('Too many arguments')
 
-    if filename != None:
+    if in_filename != None:
         try:
-            file = open(file, 'r')
+            in_file = open(in_filename, 'r')
         except FileNotFoundError:
-            panic('Failed to open input file')
+            panic('Failed to open input file for reading')
     else:
-        file = sys.stdin
+        in_file = sys.stdin
 
-    points = read_points_from_csv(file)
+    if out_filename != None:
+        try:
+            out_file = open(out_filename, 'w')
+        except OSError:
+            panic('Failed to open output file for writing')
+    else:
+        out_file = sys.stdout
+
+    points = read_points_from_csv(in_file)
     file.close()
     
+    smoothed = smooth(degree, points, trials, steps, max_perturb, window, max_subprocs,
+        smooth_procs)
+
+    out_file.writelines(f'{point[0]},{point[1]}\n' for point in smoothed)
 
 if __name__ == '__main__':
     _run_cli()
