dce67eec1675a390033cf92d8a6830a5509da8c8
Eddy Meals <edimea211@gmail.com>
2025-12-23 21:10:13 -0800

    Test smoothing on data from weird composite function

--- test_data_gen.py
+++ test_data_gen.py
@@ -5,7 +5,7 @@ import random
 def main():
     seed = 42
     coeffs = (200, -39375, -425, 55, 1)
-    noise_size = 200000
+    noise_size = 150000
     step_size = 3/16/2
     data_length = 400
 
@@ -21,8 +21,10 @@ def main():
 
     for i in range(data_length):
         x = (i - 60) * step_size
+        poly = poly_eval(coeffs, x)
+        spike = (-100*(4*x - 62)*(4.2*x - 63)*(7*x - 64)*(3*x - 65) + 200000) / ((x - 15)**4 + 1)
         noise = (random.random() * 2 - 1) * noise_size
-        print(f'{x},{poly_eval(coeffs, x) + noise}')
+        print(f'{x},{poly + spike + noise}')
 
 if __name__ == '__main__':
     main()
